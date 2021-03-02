394ffff48a9eee1644f892f954a5bbd6cd7283e8
hishmatrai <hishmatrai.2018@gmail.com>
2021-03-02 17:07:34 +0500

    update

--- src/screens/Search/Search.js
+++ src/screens/Search/Search.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, StatusBar, Image, TextInput, ScrollView } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
-import { EvilIcons } from '@expo/vector-icons';
-import { Entypo } from '@expo/vector-icons';
-import { FontAwesome } from '@expo/vector-icons';
-import { Feather } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
+import { Ionicons, Entypo } from '@expo/vector-icons';
 export default function Search(props) {
+    let RecentSerach = [
+        { userPofile: "", userName: "Jarvis Pepperspray", mutualFriend: "5 mutual friends", buttonValue: "Following" },
+        { userPofile: "", userName: "Texas", mutualFriend: "100k followers", buttonValue: "Follow" },
+        { userPofile: "", userName: "Italy", mutualFriend: "900k followers", buttonValue: "Follow" },
+        { userPofile: "", userName: "Druid Wensleydale", mutualFriend: "7 mutual friends", buttonValue: "Following" },
+        { userPofile: "", userName: "Parsley Montana", mutualFriend: "2 mutual friends", buttonValue: "Following" },
+        { userPofile: "", userName: "Linguina Nettlewater", mutualFriend: "12 mutual friends", buttonValue: "Follow" },
+        { userPofile: "", userName: "Carnegie Mondover", mutualFriend: "5 mutual friends", buttonValue: "Following" },
+
+    ]
     return (
         <View style={styles.container}>
             <StatusBar barStyle="white" hidden={false} backgroundColor="#33CC66" translucent={true} />
@@ -41,23 +45,30 @@ export default function Search(props) {
             {/* >>>>>>>>>>>>>>>>> BODY <<<<<<<<<<<<<<<<<<< */}
             <ScrollView>
                 <View style={{ margin: 20 }}>
-                    <View style={styles._main}>
-                        <View style={styles._icons_main}>
-                            <View style={styles.Profile_img}>
-                                <Ionicons name="chatbubbles" size={24} color="white" />
+                    {RecentSerach.map((v, i) => {
+                        return (
+                            <View style={styles._main}>
+                                <View style={styles._icons_main}>
+                                    <View style={styles.Profile_img}>
+                                        <Image source={require('./../../images/Mask.png')} style={styles._profile} />
+                                    </View>
+                                </View>
+                                <View style={styles._heading}>
+                                    <Text style={styles._setting_heading}>{v.userName}</Text>
+                                    <Text style={styles._mutal_frnd}>{v.mutualFriend}</Text>
+                                </View>
+                                <View style={styles._toglebtn}>
+                                    <TouchableOpacity style={[v.buttonValue === "Following" ? styles._following_btn : styles._follow_btn]}>
+                                        <Text style={[v.buttonValue === "Following" ? styles._following_btn_text : styles._follow_btn_text]}>
+
+                                            {v.buttonValue}
+                                        </Text>
+                                    </TouchableOpacity>
+                                </View>
                             </View>
-                        </View>
-                        <View style={styles._heading}>
-                            <Text style={styles._setting_heading}>Jarvis Pepperspray</Text>
-                            <Text>5 mutual friends</Text>
-                        </View>
-                        <View style={styles._toglebtn}>
-                            <TouchableOpacity style={styles._following_btn}>
-                                <Text style={styles._following_btn_text}>Following</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                    <View style={{ marginBottom: 100 }}></View>
+                        )
+                    })}
+
                 </View>
             </ScrollView>
             {/* >>>>>>>>>>>>>>>>>>>> Tab bar <<<<<<<<<<<<<<<<<< */}
@@ -106,117 +117,42 @@ const styles = StyleSheet.create({
         letterSpacing: 1,
 
     },
-    _cross_icon: {
-        justifyContent: "flex-end"
-    },
-
-    _tapbar_main: {
-        position: "absolute",
-        bottom: 0,
-        flexDirection: "row",
-        justifyContent: "space-around",
-        width: "100%",
-        alignSelf: "center",
-        alignItems: "center",
-        paddingBottom: 10,
-        paddingTop: 10,
-        borderTopColor: "gray",
-        borderTopWidth: 1,
-        backgroundColor: "white"
-    },
-    _bottom_icons: {
-        padding: 10,
-    },
     _icons_main: {
-        width: "20%",
-        // backgroundColor: "red"
+        width: "10%",
     },
     _heading: {
-        width: "50%",
-        // backgroundColor: "green"
+        width: "45%",
     },
     _toglebtn: {
-        width: "30%",
-        alignSelf: "center",
-        alignItems: "center",
-        backgroundColor: "blue"
+        width: "35%",
+        alignItems: "center"
     },
     _main: {
         flexDirection: "row",
         justifyContent: "space-between",
-        alignItems: "center"
-    },
-    _icons_box: {
-        borderRadius: 100,
-        backgroundColor: "#707070",
-        width: 50,
-        height: 50,
         alignItems: "center",
-        justifyContent: "center"
-    },
-    _icons_box2: {
-        borderRadius: 100,
-        backgroundColor: "#2CB9B0",
-        width: 50,
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center"
+        marginBottom: 30
     },
     Profile_img: {
         borderRadius: 100,
-        backgroundColor: "#5672FF",
-        width: 50,
-        height: 50,
+        width: 40,
+        height: 40,
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "center",
+        alignItems: "center"
     },
-    _icons_box4: {
+    _profile: {
+        width: 40,
+        height: 40,
+        alignItems: "center",
         borderRadius: 100,
-        backgroundColor: "#FF568A",
-        width: 50,
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center"
+
     },
-    _icons_box5: {
-        borderRadius: 100,
-        backgroundColor: "#8C61C5",
-        width: 50,
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    _icons_box6: {
-        borderRadius: 100,
-        backgroundColor: "#4682B4",
-        width: 50,
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    _icons_box7: {
-        borderRadius: 100,
-        backgroundColor: "#FFC041",
-        width: 50,
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    _icons_box8: {
-        borderRadius: 100,
-        backgroundColor: "#FF6E6E",
-        width: 50,
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center"
-    },
+
     _setting_heading: {
-        color: "#707070",
-        fontWeight: 'bold'
-    },
-    _setting_heading2: {
-        color: "#FF6E6E",
-        fontWeight: 'bold'
+        color: "#6F8BA4",
+        fontWeight: 'bold',
+        letterSpacing: 1
     },
     _search_history_main: {
         flexDirection: "row",
@@ -236,18 +172,45 @@ const styles = StyleSheet.create({
         letterSpacing: 1,
         fontSize: 15
     },
-    _following_btn:{
-        backgroundColor:"#33CC66",
-        alignItems:"center",
-        padding:10,
-        borderRadius:20
+    _following_btn: {
+        backgroundColor: "#33CC66",
+        alignItems: "center",
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingRight: 20,
+        paddingLeft: 20,
+        borderRadius: 20,
+        width: 110
     },
-    _following_btn_text:{
-        color:"white",
-        fontWeight:"bold",
-        letterSpacing:2,
-        fontSize:13
+    _following_btn_text: {
+        color: "white",
+        fontWeight: "bold",
+        letterSpacing: 2,
+        fontSize: 13
 
-    }
+    },
+    _mutal_frnd: {
+        color: "#C8D4DF",
+        letterSpacing: 1,
+        fontWeight: 'bold'
+    },
+    _follow_btn_text: {
+        color: "#33CC66",
+        fontWeight: "bold",
+        letterSpacing: 2,
+        fontSize: 13
+
+    },
+    _follow_btn: {
+        borderColor: "#33CC66",
+        borderWidth: 1,
+        alignItems: "center",
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingRight: 20,
+        paddingLeft: 20,
+        borderRadius: 20,
+        width: 110
+    },
 
 });
